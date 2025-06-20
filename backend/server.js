ffc15f1ebc92c5d49de64823896bb26a06e787be
Nezrah <qarroum.adil@gmail.com>
2025-06-20 03:11:00 +0200

    fix battlenet 2

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ passport.use('battlenet', new BattlenetStrategy({
 
 // 3. Routes d'authentification
 app.get('/auth/battlenet',
-  passport.authenticate('battlenet')
+  passport.authenticate('battlenet', { state: true })
 );
 app.get('/auth/battlenet/callback',
   passport.authenticate('battlenet', { failureRedirect: '/' }),
